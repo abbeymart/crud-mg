6d51db15ffe59eedc9068d18e446e2549f0b65f6
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2020-05-03 22:51:56 -0400

    successfully tested GetAllRecord crud API

--- test/tests.GetAllRecord.js
+++ test/tests.GetAllRecord.js
@@ -1,5 +1,5 @@
 /**
- * @Author: abbeymart | Abi Akindele | @Created: 2019-06-06 | @Updated: 2019-06-12
+ * @Author: abbeymart | Abi Akindele | @Created: 2019-06-06 | @Updated: 2019-05-03
  * @Company: mConnect.biz | @License: MIT
  * @Description: @mconnect/crud testing, GetAllRecord
  */
@@ -7,8 +7,8 @@
 const {suite, test, before} = require('mocha');
 const ok                    = require('./assert');
 
-const {dbConnect}  = require('./mgConnect');
-const GetAllRecord = require('../src/GetAllRecord');
+const {dbConnect}    = require('./mgConnect');
+const {newGetAllRecord, GetAllRecord} = require('../src/GetAllRecord');
 
 let params,
     options = {};
@@ -26,40 +26,40 @@ suite('@mconnect/crud package Testing - GetAllRecord:', () => {
             ok(typeof res === 'object', `response should be an object: ${res}`);
         });
         test('should return valid instance record, with function-call', () => {
-            const res = GetAllRecord(dbConnect, params, options);
+            const res = newGetAllRecord(dbConnect, params, options);
             // console.log('result-call instance: ', res);
             ok(typeof res === 'object', `response should be an object: ${res}`);
         });
         test('should return valid # of records', async () => {
-            const resInstance = GetAllRecord(dbConnect, params);
+            const resInstance = newGetAllRecord(dbConnect, params);
             const res         = await resInstance.getAllRecord();
             // console.log('result-records #: ', res);
             ok(res.value.length > 0, `response should be a non-empty array: ${res.value.length}`);
         });
         test('should return valid # of records from cache', async () => {
-            const resInstance = GetAllRecord(dbConnect, params);
+            const resInstance = newGetAllRecord(dbConnect, params);
             const res         = await resInstance.getAllRecord();
             // console.log('result-records: ', res.value[0]);
             ok((res.value.length > 0 && res.message === 'from cache'), `response should be a non-empty array: ${res.value.length}`);
         });
         test('should return valid # of records, by queryParams', async () => {
-            params = {
-                coll: 'locations',
+            params            = {
+                coll       : 'locations',
                 queryParams: {
                     code: 'US'
                 }
             };
-            const resInstance = GetAllRecord(dbConnect, params);
+            const resInstance = newGetAllRecord(dbConnect, params);
             const res         = await resInstance.getAllRecord();
             // console.log('result-records #: ', res);
-            ok(res.value.length === 1, `response should be a non-empty array: ${res.value.length}`);
+            ok(res.value.length >= 1, `response should be a non-empty array: ${res.value.length}`);
         });
     });
 
     suite('Negative testing:', () => {
         test('should return paramsError, with null appDb', async () => {
-            const resInstance = GetAllRecord('', params, options);
-            const res         = await resInstance.getAllRecord();
+            const resInstance = newGetAllRecord('', params, options);
+            const res         = await resInstance.getAllRecord() || {};
             ok(res['code'] === 'paramsError', `response should be a function: ${res['code']}`);
         });
     });
